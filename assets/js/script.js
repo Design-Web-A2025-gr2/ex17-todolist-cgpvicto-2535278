115ed0b052ff9bb86fcb4d070734d9ab345ab784
cgpvicto-2535278 <2535278@etudiant.cegepvicto.ca>
2025-11-07 09:07:08 -0500

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,19 @@ var boutonTache = document.getElementById("boutonAjout");
 var saisieTache = document.getElementById("saisie-tache");
 var listeAFaire = document.getElementById("listeAFaire");
 var listeFait = document.getElementById("listeFait");
+var poubelle = document.querySelector("icone-trash");
 
 // ====================================================
 // =  Déclaration des événements                      =
 // ====================================================
 
 boutonTache.addEventListener("click", ajouterTache);
+saisieTache.addEventListener("keydown", function(t) {
+    if(t.key === 'Enter'){
+        ajouterTache();
+    }
+});
+poubelle.addEventListener("click", clearTaches);
 
 // ====================================================
 // =  Code qui sera exécuté au chargement de la page  =
@@ -23,16 +30,25 @@ boutonTache.addEventListener("click", ajouterTache);
 // =  Déclaration des fonctions                       =
 // ====================================================
 
+/**
+ * Ajouter une tache a la liste de taches a faire
+ */
 function ajouterTache(){
     if (saisieTache.value != ""){
-        var tache = document.createElement("li");
+        let tache = document.createElement("li");
         tache.innerText = saisieTache.value;
-        tache.classList.add("tache");
+        tache.className = "tache";
         listeAFaire.append(tache);
-        tache.addEventListener("click", enleverTache);
+        saisieTache.value = "";
+        //tache.addEventListener("click", enleverTache);
     }
 }
 
 function enleverTache(){
     tache.classList.add("fait");
+    listeFait.append(tache);
+}
+
+function clearTaches(){
+    listeFait.
 }
